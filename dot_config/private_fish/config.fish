48b5745ed4930ebbb34448477b378f84937b6394
Nicolas Saavedra <87380139+Nicolas-Saavedra@users.noreply.github.com>
2025-08-01 09:50:07 -0500

    feat!: set editor and edit alias for fish
    merge
    
    fixup
    
    fixup

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,8 +1,10 @@
 if status is-interactive
   set fish_greeting
+  set -Ux EDITOR /usr/bin/nvim
   set -Ux GOPATH $HOME/.cache/go
   set -Ux GOBIN $HOME/.local/bin
-  alias lg lazygit
-  alias tmux zellij
+  alias lg='lazygit'
+  alias edit='chezmoi edit'
+  alias tmux='zellij'
   starship init fish | source
 end
