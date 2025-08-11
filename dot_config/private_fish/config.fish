862022a034e2d65d3819e7ebc7a8021fbdb1af2e
Nicolas Saavedra <87380139+Nicolas-Saavedra@users.noreply.github.com>
2025-08-11 12:22:00 -0500

    feat!: added private fish config for env vars

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,11 @@ if status is-interactive
   set -Ux EDITOR /usr/bin/nvim
   set -Ux GOPATH $HOME/.cache/go
   set -Ux GOBIN $HOME/.local/bin
+
+  if test -f ~/.config/private/config.local.fish
+    source ~/.config/private/config.local.fish
+  end
+
   alias lg='lazygit'
   alias edit='chezmoi edit'
   alias tmux='zellij'
