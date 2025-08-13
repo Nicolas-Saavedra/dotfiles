89da8a9e65f28b8e710f55446f67d2b2d5aa0c32
Nicolas Saavedra <87380139+Nicolas-Saavedra@users.noreply.github.com>
2025-08-13 09:34:23 -0500

    fix: update settings to allow floating windows to be transparent as well

--- dot_config/nvim/lua/custom/plugins/catppuccin.lua
+++ dot_config/nvim/lua/custom/plugins/catppuccin.lua
@@ -7,6 +7,10 @@ return {
     ---@diagnostic disable-next-line: missing-fields
     require('catppuccin').setup {
       transparent_background = true,
+      float = {
+        transparent = true,
+        solid = false, -- use solid styling for floating windows, see |winborder|
+      },
     }
 
     vim.cmd.colorscheme 'catppuccin-mocha'
